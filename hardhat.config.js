32b82991142441e7c782c2497e2581e0854de9fe
CodingCas <casweeno2000@gmail.com>
2022-07-16 02:37:41 +0100

    Contract verified on etherscan rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan")
 require("dotenv").config({ path: ".env" });
 
 const ALCHEMY_RINKEBY_API_KEY_URL = process.env.ALCHEMY_RINKEBY_API_KEY_URL;
@@ -8,6 +9,8 @@ const INFURA_ROPSTEN_API_KEY_URL = process.env.INFURA_ROPSTEN_API_KEY_URL;
 const MORALIS_ROPSTEN_API_KEY_URL = process.env.MORALIS_ROPSTEN_API_KEY_URL;
 
 const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+
+const API_TOKEN = process.env.API_TOKEN;
 
 module.exports = {
   solidity: "0.8.1",
@@ -21,4 +24,7 @@ module.exports = {
       accounts: [ACCOUNT_PRIVATE_KEY],
     }
   },
+  etherscan: {
+    apiKey: API_TOKEN
+  }
 };
